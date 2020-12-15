437fecbce7ff6e233ca05b58729db9222e166ab6
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-15 13:35:23 -0800

    Added navbar with home and favorite buttons

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function getMovie(name) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://www.omdbapi.com/?t=' + name + '&apikey=6bc8c31e');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+  })
+  xhr.send();
+}
+getMovie('Avengers%3A+Infinity+War');

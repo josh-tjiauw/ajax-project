2d22d9e353319afed626d8342677904d74a8e21d
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-16 00:52:48 -0800

    Made the XMLHttpRequest event listener assign the movie's descriptions in data.js.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+var numMovie = 1;
+var arrayOfMovies = ['Avengers: Infinity War', 'Tenet', 'Underwater', 'Terminator 2: Judgment Day'];
 function getMovie(name) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://www.omdbapi.com/?t=' + name + '&apikey=6bc8c31e');
   xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    console.log(xhr.status);
-    console.log(xhr.response);
-  })
   xhr.send();
-
+  xhr.addEventListener('load', function () {
+    movie.title = xhr.response.Title;
+    movie.year = xhr.response.Year;
+    movie.ratings = xhr.response.Ratings[0];
+    movie.length = xhr.response.Runtime;
+    movie.genre = xhr.response.Genre;
+    movie.actors = xhr.response.Actors;
+    movie.description = xhr.response.Plot;
+  })
 }
 
-getMovie('Avengers: Infinity War');
+getMovie('Avengers: Infinity War')
+console.log(movie);

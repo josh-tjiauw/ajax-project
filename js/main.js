ec07bf88c38459eaa13e2de2e32c1b89164849b2
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-03 07:50:43 -0800

    Converted the named functions to es6 style, and converted all var's to const/let

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-var $viewNodeList = document.getElementsByClassName('view');
-var $username = document.getElementById('user-name');
-var $greeting = document.getElementById('greeting')
-var getMovieCounter = 0;
-var numMovie = 0;
-var numFavs = 0;
-var arrayOfMovies = ['Avengers: Infinity War', 'Tenet', 'Underwater', 'Terminator 2: Judgment Day'];
-var arrayAction = [];
-var $form = document.querySelector('form');
-var $action = document.getElementById('action-genre');
-var $container = document.querySelector('.container');
-var $homeNav = document.getElementById('home-button');
-var $favoritesNav = document.getElementById('favorites-button')
-var $favoritesPage = document.getElementById('favorites')
-var $favoritesContainer = document.getElementById('favorites-container')
+const $viewNodeList = document.getElementsByClassName('view');
+const $username = document.getElementById('user-name');
+const $greeting = document.getElementById('greeting')
+let getMovieCounter = 0;
+let numMovie = 0;
+let numFavs = 0;
+const arrayOfMovies = ['Avengers: Infinity War', 'Tenet', 'Underwater', 'Terminator 2: Judgment Day'];
+const arrayAction = [];
+const $form = document.querySelector('form');
+const $action = document.getElementById('action-genre');
+const $container = document.querySelector('.container');
+const $homeNav = document.getElementById('home-button');
+const $favoritesNav = document.getElementById('favorites-button')
+const $favoritesPage = document.getElementById('favorites')
+const $favoritesContainer = document.getElementById('favorites-container')
 
 $form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -23,14 +23,14 @@ $form.addEventListener('submit', (event) => {
   $greeting.textContent = "Hello " + data.username + "!";
 })
 
-var $dataViewNodeList = [];
-for (i = 0; i < $viewNodeList.length; i++) {
-  var dvnode = $viewNodeList[i].getAttribute('data-view');
+const $dataViewNodeList = [];
+for (let i = 0; i < $viewNodeList.length; i++) {
+  const dvnode = $viewNodeList[i].getAttribute('data-view');
   $dataViewNodeList.push(dvnode);
 }
 
-function viewSwap(dataview) {
-  for (i = 0; i < $viewNodeList.length; i++) {
+let viewSwap = (dataview) => {
+  for (let i = 0; i < $viewNodeList.length; i++) {
     if (dataview !== $dataViewNodeList[i]) {
       $viewNodeList[i].className = 'view hidden';
     }
@@ -63,22 +63,22 @@ document.addEventListener('click', (event) => {
 
 viewSwap('login');
 
-function getMovie(name) {
-  var xhr = new XMLHttpRequest();
+let getMovie = (name) => {
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://www.omdbapi.com/?t=' + name + '&apikey=6bc8c31e');
   xhr.responseType = 'json';
   xhr.send();
-  xhr.addEventListener('load', function () {
-    var $movieContainer = document.getElementById(xhr.response.imdbID);
-    var $movieImg = document.createElement('img');
-    var $movieImgContainer = document.createElement('div');
-    var $movieTitle = document.createElement('h1');
-    var $movieRatings = document.createElement('h2');
-    var $movieLength = document.createElement('h2');
-    var $movieDescription = document.createElement('p');
-    var $favButtonContainer = document.createElement('div');
-    var $addToFavorites = document.createElement('button');
-    var $removeFromFavorites = document.createElement('button')
+  xhr.addEventListener('load', () => {
+    const $movieContainer = document.getElementById(xhr.response.imdbID);
+    const $movieImg = document.createElement('img');
+    const $movieImgContainer = document.createElement('div');
+    const $movieTitle = document.createElement('h1');
+    const $movieRatings = document.createElement('h2');
+    const $movieLength = document.createElement('h2');
+    const $movieDescription = document.createElement('p');
+    const $favButtonContainer = document.createElement('div');
+    const $addToFavorites = document.createElement('button');
+    const $removeFromFavorites = document.createElement('button')
 
     $addToFavorites.addEventListener('click', () => {
       $addToFavorites.className = 'hidden';
@@ -135,20 +135,20 @@ function getMovie(name) {
   })
 }
 
-var $empty = document.createElement('h1');
+const $empty = document.createElement('h1');
 $empty.className = 'subheader-text center-title';
 $empty.innerHTML = 'Your Favorites List is Empty.<br><br>Add movies from the Home Page!<br><br>';
-var $emptyHome = document.createElement('button');
+const $emptyHome = document.createElement('button');
 $emptyHome.id = 'return-home';
 $emptyHome.textContent = 'Return to Home Page';
-var $homeDataView = document.createAttribute('data-view')
+const $homeDataView = document.createAttribute('data-view')
 $homeDataView.value = 'home'
 $emptyHome.setAttributeNode($homeDataView);
 $empty.appendChild($emptyHome);
 $favoritesContainer.appendChild($empty);
 
-function returnFavoritesIndex() {
-  for(var i = 0; i < data.favorites.length; i++){
+let returnFavoritesIndex = () => {
+  for(let i = 0; i < data.favorites.length; i++){
     if(data.view === data.favorites[i].imdbID){
       return i;
     }
@@ -156,10 +156,10 @@ function returnFavoritesIndex() {
   return i;
 }
 
-function removeFavorites() {
-  var index = returnFavoritesIndex();
-  var $colfourtharray = document.querySelectorAll('.col-fourth');
-  for (var i = 0; i < $colfourtharray.length; i++) {
+let removeFavorites = () => {
+  const index = returnFavoritesIndex();
+  const $colfourtharray = document.querySelectorAll('.col-fourth');
+  for (let i = 0; i < $colfourtharray.length; i++) {
     if ($colfourtharray[i].id === data.view){
       $colfourtharray[i].remove()
     }
@@ -168,15 +168,15 @@ function removeFavorites() {
   numFavs--;
 }
 
-function displayFavorites() {
-  for(var i = numFavs; i<data.favorites.length; i++){
-    var $colfourth = document.createElement('div')
+let displayFavorites = () => {
+  for(let i = numFavs; i<data.favorites.length; i++){
+    const $colfourth = document.createElement('div')
     $colfourth.className = 'col-fourth';
     $colfourth.id = data.favorites[i].imdbID;
-    var $colfourthimg = document.createElement('img');
+    const $colfourthimg = document.createElement('img');
     $colfourthimg.src = data.favorites[i].Poster;
     $colfourthimg.alt = data.favorites[i].Title + ' Poster';
-    var $dataview = document.createAttribute('data-view');
+    const $dataview = document.createAttribute('data-view');
     $dataview.value = data.favorites[i].imdbID;
     $colfourthimg.setAttributeNode($dataview);
     $colfourth.appendChild($colfourthimg);
@@ -186,15 +186,15 @@ function displayFavorites() {
 }
 
 
-function display(genre) {
+let display = (genre) => {
   if (genre === 'action') {
     for (var i = numMovie; i < arrayAction.length; i++) {
-      var $colfourth = document.createElement('div')
+      const $colfourth = document.createElement('div')
       $colfourth.className = 'col-fourth';
-      var $colfourthimg = document.createElement('img');
+      const $colfourthimg = document.createElement('img');
       $colfourthimg.src = arrayAction[i].Poster;
       $colfourthimg.alt = arrayAction[i].Title + ' Poster';
-      var $dataview = document.createAttribute('data-view');
+      const $dataview = document.createAttribute('data-view');
       $dataview.value = arrayAction[i].imdbID;
       $colfourthimg.setAttributeNode($dataview);
       $colfourth.appendChild($colfourthimg);
@@ -204,18 +204,18 @@ function display(genre) {
   }
 }
 
-for(var i=getMovieCounter; i<arrayOfMovies.length; i++){
+for(let i=getMovieCounter; i<arrayOfMovies.length; i++){
   getMovie(arrayOfMovies[i]);
   getMovieCounter++
 }
 
-var previousDataJSON = localStorage.getItem('data');
+const previousDataJSON = localStorage.getItem('data');
 if (previousDataJSON !== null) {
   data = JSON.parse(previousDataJSON);
 }
 
 window.addEventListener('beforeunload', (event) => {
   event.preventDefault();
-  var dataJSON = JSON.stringify(data);
+  const dataJSON = JSON.stringify(data);
   localStorage.setItem('movie', dataJSON);
 })
